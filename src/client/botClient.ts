19ddd560f4144f45d013f21cd29ba59f39baeabf
Westlanderz <noah28129912@gmail.com>
2021-01-07 14:41:30 +0000

    Code on on my desktop

--- src/client/botClient.ts
+++ src/client/botClient.ts
@@ -2,3 +2,30 @@ import { AkairoClient, CommandHandler, ListenerHandler } from "discord-akairo";
 import { User, Message } from "discord.js";
 import { join } from "path";
 import { prefix, owners } from "../Config";
+
+declare module "discord-akairo" {
+    interface AkairoClient {
+        commandHandler: CommandHandler;
+        listenerHandler: ListenerHandler;
+    }
+}
+
+interface BotOptions {
+    token?: string;
+    owners?: string | string[];
+}
+
+export default class BotClient extends AkairoClient {
+    public config: BotOptions;
+    public listenerHandler: ListenerHandler = new ListenerHandler(this, {
+        directory: join(__dirname, "..", "listeners")
+    });
+
+    public commandHandler: CommandHandler = new CommandHandler(this, {
+        directory: join(__dirname, "..", "commands"),
+        prefix: prefix,
+        allowMention: true,
+        handleEdits: true,
+        
+    })
+}
