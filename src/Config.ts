fe02bed994654ef0daca0b507ea8e210ba37ed38
root <root@vmi485021.contaboserver.net>
2021-01-10 12:14:52 +0100

    Made sure bot works now

--- src/Config.ts
+++ src/Config.ts
@@ -1,3 +1,18 @@
-export const token: string = "";
-export const prefix: string = "!";
-export const owners: string[] = [""];
+/**
+ * Discord bot config.
+ *
+ * Revisions to this file should not be committed to the repository.
+ */
+export type BotConfig = {
+    token: string;
+    prefix: string;
+    botOwnerRoleName: string;
+    enableReactions: boolean;
+};
+
+export const config: BotConfig = {
+    token: '', // TODO: Put your token here!
+    prefix: '!', // Command prefix. ex: !help
+    botOwnerRoleName: 'bot-owner',
+    enableReactions: true,
+};
