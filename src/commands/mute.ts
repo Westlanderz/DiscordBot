8530364efd658c128e7d482b9e6a0fe3ba2acac0
Westlanderz <admin@noahknegt.com>
2021-01-12 20:30:13 +0100

    Combined mute commands to one file

--- src/commands/mute.ts
+++ src/commands/mute.ts
@@ -23,3 +23,26 @@ export class MuteCommand implements Command {
         return false;
     }
 }
+
+export class UnmuteCommand implements Command {
+    commandNames = ['unmute'];
+
+    getHelpMessage(commandPrefix: string): string {
+        return `Use ${commandPrefix}unmute [user] to unmute a certain user.`
+    }
+
+    async run(parsedUserCommand: CommandContext): Promise<void> {
+        const member = parsedUserCommand.originalMessage.mentions.members.first();
+        if (member){
+            const mutedMember = await member.voice.setMute(false);
+            console.log(mutedMember.user.username);
+            await parsedUserCommand.originalMessage.channel.send(`${mutedMember.user.username} has been unmuted.`);
+        }
+    }
+
+    hasPermissionToRun(parsedUserCommand: CommandContext): boolean {
+        if (parsedUserCommand.originalMessage.member.hasPermission(["MUTE_MEMBERS"]) || parsedUserCommand.originalMessage.member.hasPermission(["ADMINISTRATOR"]))
+            return true;
+        return false;
+    }
+}
