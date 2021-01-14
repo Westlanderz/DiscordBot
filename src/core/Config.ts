a2e53a4b405d66d0aac13cdd18913abb028d8e4c
Westlanderz <noah28129912@gmail.com>
2021-01-14 21:10:43 +0100

    Rewrite everything xd

---
+++ src/core/Config.ts
@@ -0,0 +1,11 @@
+class ConfigMeta {
+    private type;
+
+    constructor(type) {
+        this.type = type;
+    }
+
+    private call(module_name: string, unique_id: string, force_registration: boolean = false, ) {
+        
+    }
+}
