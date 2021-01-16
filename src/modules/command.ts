b697fe046170e12322df75786ed80046e7443749
Westlanderz <noah28129912@gmail.com>
2021-01-16 20:31:03 +0000

    Removed commants

--- src/modules/command.ts
+++ src/modules/command.ts
@@ -1,11 +1,6 @@
 import { CommandContext } from '../commandContext';
 
 export interface Command {
-    /**
-     * List of aliases for the command.
-     *
-     * The first name in the list is the primary command name.
-     */
     readonly commandNames: string[];
     getHelpMessage(commandPrefix: string): string;
     run(parsedUserCommand: CommandContext): Promise<void>;
