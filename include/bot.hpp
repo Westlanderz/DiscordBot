97ee20a957fc01e7e016bdad94cbf5f66369efe2
Westlanderz <noah28129912@gmail.com>
2021-05-30 21:47:55 +0200

    initial bot class

---
+++ include/bot.hpp
@@ -0,0 +1,43 @@
+#ifndef bot_hpp
+#define bot_hpp
+
+#include <vector>
+#include <map>
+#include "include.hpp"
+
+class CommandHandler;
+class Module;
+
+class Bot {
+    private:
+        std::shared_ptr<DppBot> bot;
+        std::string username;
+        std::string defaultPrefix;
+        std::vector<dpp::Guild> servers;
+        std::map<dpp::Guild, CommandHandler *> commandhandlers;
+        std::string token;
+        std::string botOwnerRole;
+        bool enableReactions;
+    
+    public:
+        json self;
+        std::vector<Module *> modules;
+
+        Bot(std::string, std::string);
+        virtual ~Bot();
+
+        void login(std::string);
+        void setIntents(uint16_t intents);
+        void initServerJoiner();
+        void initModules();
+        void run();
+        CommandHandler * isCommandHandler(dpp::Guild);
+        DppBot * hasDpp();
+        std::string isPrefix();
+        void addCommandHandler(dpp::Guild);
+        void removeCommandHandler(dpp::Guild);
+        void sendMessage(dpp::snowflake);
+        void sendMessage(dpp::User);
+};
+
+#endif
