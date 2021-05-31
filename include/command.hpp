4b42d166480f1358eb7f194095e0109cb5c57f79
Westlanderz <noah28129912@gmail.com>
2021-05-31 17:38:02 +0200

    Updated the command class

--- include/command.hpp
+++ include/command.hpp
@@ -11,21 +11,27 @@ class Command {
         Module *module;
 
     public:
-        Command(std::vector<std::string> names) {
+        Command(std::vector<std::string> names, Module *module) {
             this->names = names;
+            this->module = module;
         }
         virtual ~Command() = default;
 
-        virtual std::string getHelpMessage();
-        virtual void execute(dpp::Message);
-        virtual bool hasPermsToRun(dpp::User) { return true; };
-        virtual bool isName(std::string name) {
+        virtual std::string getHelpMessage() = 0;
+        virtual void execute(dpp::Message) = 0;
+        virtual bool hasPermsToRun(dpp::User user = NULL) { return true; };
+        virtual bool isName(std::string name = "") {
+            if(!name.compare("")) {
+                name = names.at(0);
+            }
             for(auto it = names.begin(); it != names.end(); it++) {
                 if(!it->compare(name))
                     return true;
             }
             return false;
         };
+        virtual Module * isModule() { return module; };
+        virtual std::string getName() { return names.at(0); };
 };
 
 #endif
