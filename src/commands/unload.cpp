749f5055de13ea6cf3c25657e759fe4a465e9e63
Westlanderz <noah28129912@gmail.com>
2021-06-04 21:19:50 +0200

    Added unload command

---
+++ src/commands/unload.cpp
@@ -0,0 +1,64 @@
+#include "../../include/commands/unload.hpp"
+#include "../../include/module.hpp"
+#include "../../include/commandhandler.hpp"
+#include "../../include/bot.hpp"
+#include "../../include/commandexception.hpp"
+
+UnLoad::UnLoad(std::vector<std::string> names): Command(names) {}
+
+std::string UnLoad::getHelpMessage() {
+    return "Use " + module->isHandler()->isPrefix() + names[0] + " <module> to unload that module.";
+}
+
+void UnLoad::execute(dpp::Message msg) {
+    try {
+        std::size_t find_args = msg.content->find(" ");
+        std::size_t find_command = msg.content->find(" ", find_args + 1);
+        std::string module_name = "";
+        if(find_args != std::string::npos) {
+            if(find_command != std::string::npos) {
+                module_name = msg.content->substr(find_args + 1, find_command - find_args);
+            } else {
+                module_name = msg.content->substr(find_args + 1);
+            }
+            if(!module_name.compare("core"))
+                throw CommandException("Can not unload " + module_name, EXECUTE_ERROR, 0);
+            for(auto &_module : module->isHandler()->getModules()) {
+                if(_module->isName(module_name) && _module->isLoaded()) {
+                    module->isHandler()->unloadModule(_module);
+                    module->isHandler()->hasBot()->sendMessage(*msg.channel_id, false, "Unloaded the " + _module->getName() + " module for you.");
+                    std::cout << "\033[1;32mExecuted " + this->getName() + "\033[0m" << std::endl;
+                } else {
+                    module->isHandler()->hasBot()->sendMessage(*msg.channel_id, false, "Unable to find this module or this module is not yet loaded.");
+                    throw CommandException("Could not execute " + this->getName(), EXECUTE_ERROR, 0);
+                }
+            }
+        } else {
+            module->isHandler()->hasBot()->sendMessage(*msg.channel_id, false, "You have not given a module to unload. " + this->getHelpMessage());
+            throw CommandException("Could not execute " + this->getName(), PARAM_ERROR, 0);
+        }
+    } catch(CommandException &e) {
+        std::cerr << "\033[1;31m" << e.what() << " \033[1;35mwith error code \033[1;36m" << e.getErrorNumber() << " \033[0m" << std::endl;
+        module->isHandler()->setLastException(&e);
+    }
+}
+
+bool UnLoad::hasPermsToRun(dpp::User user) {
+    std::vector<dpp::snowflake> admin = module->isHandler()->adminRoles();
+    std::vector<Module *> modules = module->isHandler()->getModules();
+    dpp::Guild guild = module->isHandler()->isFromGuild();
+    bool isAdmin = false;
+    std::string authorid = user["id"].get<std::string>();
+    for(std::size_t i = 0; i < guild.at(0)["members"].size(); ++i) {
+        if(guild.at(0)["members"].at(i)["user"]["id"] == authorid){
+            for(std::size_t j = 0; j < guild.at(0)["members"].at(i)["roles"].size(); ++j) {
+                for(auto &role : admin) {
+                    if(guild.at(0)["members"].at(i)["roles"].at(j) == std::to_string(role))
+                        isAdmin = true;
+                }
+            }
+        }
+    }
+    std::cout << (isAdmin ? "\033[1;32mPermission granted\033[0m" : "\033[1;31mNot enough permissions\033[0m")<< std::endl;
+    return isAdmin;
+}
