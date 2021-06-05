cd3ae37d57a2902b4125dc6567897e35ac1d18bd
Westlanderz <noah28129912@gmail.com>
2021-06-05 17:36:59 +0200

    Added reload command

--- include/commands/reload.hpp
+++ include/commands/reload.hpp
@@ -1,5 +1,14 @@
 #ifndef RELOAD_HPP
 #define RELOAD_HPP
 
+#include "../command.hpp"
+
+class Reload : public Command {
+    public:
+        Reload(std::vector<std::string>);
+        std::string getHelpMessage();
+        void execute(dpp::Message);
+        bool hasPermsToRun(dpp::User) override;
+};
 
 #endif // RELOAD_HPP
---
+++ src/commands/reload.cpp
@@ -0,0 +1,88 @@
+#include "../../include/commands/reload.hpp"
+#include "../../include/module.hpp"
+#include "../../include/commandhandler.hpp"
+#include "../../include/bot.hpp"
+#include "../../include/commandexception.hpp"
+
+Reload::Reload(std::vector<std::string> names): Command(names) {}
+
+std::string Reload::getHelpMessage() {
+    return "Use " + module->isHandler()->isPrefix() + names[0] + " <module> to reload the given module.";
+}
+
+void Reload::execute(dpp::Message msg) {
+    try {
+        std::size_t find_args = msg.content->find(" ");
+        std::size_t find_command = msg.content->find(" ", find_args + 1);
+        std::string module_name = "";
+        std::string unload_msg = "";
+        std::vector<Module *> modules = module->isHandler()->getModules();
+        if(find_args != std::string::npos) {
+            if(find_command != std::string::npos) {
+                module_name = msg.content->substr(find_args + 1, find_command - find_args);
+            } else {
+                module_name = msg.content->substr(find_args + 1);
+            }
+            if(!module_name.compare("core"))
+                throw CommandException("Can not reload " + module_name, EXECUTE_ERROR, 0);
+            unload_msg = "Unable to find this module or this module is not yet loaded. These are the current loaded modules: ";
+            for(auto &_module : modules) {
+                if(_module->isName(module_name) && _module->isLoaded() && _module->getName().compare("core")) {
+                    module->isHandler()->unloadModule(_module);
+                    module->isHandler()->loadModule(_module);
+                    module->isHandler()->hasBot()->sendMessage(*msg.channel_id, false, "Reloaded the " + _module->getName() + " module for you.");
+                    std::cout << "\033[1;32mExecuted " + this->getName() + "\033[0m" << std::endl;
+                    return;
+                } else {
+                    if(_module->isLoaded() && _module->getName().compare("core")) {
+                        unload_msg.append(_module->getName());
+                        if(!_module->getName().compare(modules.at(modules.size() - 1)->getName()))
+                            unload_msg.append(". ");
+                        else
+                            unload_msg.append(", ");
+                    } else
+                        unload_msg.append(". ");
+                }
+            }
+            module->isHandler()->hasBot()->sendMessage(*msg.channel_id, false, unload_msg);
+            throw CommandException("Could not execute " + this->getName(), EXECUTE_ERROR, 0);
+        } else {
+            unload_msg = "You have not given a module to reload. Please select one of the following modules to reload: ";
+            for(auto &_module : modules) {
+                if(_module->isLoaded() && _module->getName().compare("core")) {
+                    unload_msg.append(_module->getName());
+                    if(!_module->getName().compare(modules.at(modules.size() - 1)->getName()))
+                        unload_msg.append(". ");
+                    else
+                        unload_msg.append(", ");
+                } else
+                    unload_msg.append(". ");
+            }
+            module->isHandler()->hasBot()->sendMessage(*msg.channel_id, false, unload_msg + this->getHelpMessage());
+            throw CommandException("Could not execute " + this->getName(), PARAM_ERROR, 0);
+        }
+    } catch(CommandException &e) {
+        std::cerr << "\033[1;31m" << e.what() << " \033[1;35mwith error code \033[1;36m" << e.getErrorNumber() << " \033[0m" << std::endl;
+        module->isHandler()->setLastException(&e);
+    }
+}
+
+bool Reload::hasPermsToRun(dpp::User user) {
+    std::vector<dpp::snowflake> admin = module->isHandler()->adminRoles();
+    std::vector<Module *> modules = module->isHandler()->getModules();
+    dpp::Guild guild = module->isHandler()->isFromGuild();
+    bool isAdmin = false;
+    std::string authorid = user["id"].get<std::string>();
+    for(std::size_t i = 0; i < guild.at(0)["members"].size(); ++i) {
+        if(guild.at(0)["members"].at(i)["user"]["id"] == authorid){
+            for(std::size_t j = 0; j < guild.at(0)["members"].at(i)["roles"].size(); ++j) {
+                for(auto &role : admin) {
+                    if(guild.at(0)["members"].at(i)["roles"].at(j) == std::to_string(role))
+                        isAdmin = true;
+                }
+            }
+        }
+    }
+    std::cout << (isAdmin ? "\033[1;32mPermission granted\033[0m" : "\033[1;31mNot enough permissions\033[0m")<< std::endl;
+    return isAdmin;
+}
