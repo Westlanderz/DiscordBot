7e7d33bbba0d1f09a5765e57f721b69522563f4b
Westlanderz <noah28129912@gmail.com>
2021-06-17 23:12:50 +0200

    Now reads from config file

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,20 @@
 #include "../include/bot.hpp"
+#include "../include/discordpp/log.hh"
+#include <fstream>
 
-std::string getToken();
-
-std::istream &safeGetline(std::istream &is, std::string &t);
-
-void filter(std::string &target, const std::string &pattern);
+std::string getToken(dpp::json);
 
 int main() {
     dpp::log::filter = dpp::log::info;
     dpp::log::out = &std::cerr;
 
+    std::ifstream configfile("../config.json");
+    dpp::json config = dpp::json::parse(configfile);
+    configfile.close();
+
     std::cout << "Starting bot...\n\n";
 
-    std::string token = getToken();
+    std::string token = getToken(config);
     if (token.empty()) {
         std::cerr << "CRITICAL: "
                   << "There is no valid way for Echo to obtain a token! Use "
@@ -28,80 +30,23 @@ int main() {
 
     Bot bot("DevBot", "!");
 
-    uint16_t intents = dpp::intents::DIRECT_MESSAGE_REACTIONS
-    | dpp::intents::DIRECT_MESSAGE_TYPING
-    | dpp::intents::DIRECT_MESSAGES
-    | dpp::intents::GUILD_BANS
-    | dpp::intents::GUILD_EMOJIS
-    | dpp::intents::GUILD_INTEGRATIONS
-    | dpp::intents::GUILD_INVITES
-    | dpp::intents::GUILD_MEMBERS
-    | dpp::intents::GUILD_MESSAGE_REACTIONS 
-    | dpp::intents::GUILD_MESSAGE_TYPING
-    | dpp::intents::GUILD_MESSAGES
-    | dpp::intents::GUILD_PRESENCES
-    | dpp::intents::GUILD_VOICE_STATES
-    | dpp::intents::GUILD_WEBHOOKS
-    | dpp::intents::GUILDS;
-
-    bot.setIntents(intents);
+    bot.setIntents(std::stoull(config["intents"].get<std::string>()));
     bot.initServerJoiner();
     bot.initHandlers();
-    bot.login(getToken());
+    bot.login(token);
     bot.run();
 
     return 0;
 }
 
-std::string getToken() {
+std::string getToken(dpp::json config) {
     std::string token;
     char const *env = std::getenv("BOT_TOKEN");
     if (env != nullptr) {
         token = std::string(env);
     } else {
-        std::ifstream tokenFile("../token.dat");
-        if (!tokenFile) {
-            return "";
-        }
-        safeGetline(tokenFile, token);
-        tokenFile.close();
+        token = "Bot ";
+        token.append(config["token"]);
     }
     return token;
-}
-
-/*/
- * Source: https://stackoverflow.com/a/6089413/1526048
-/*/
-std::istream &safeGetline(std::istream &is, std::string &t) {
-    t.clear();
-
-    // The characters in the stream are read one-by-one using a std::streambuf.
-    // That is faster than reading them one-by-one using the std::istream.
-    // Code that uses streambuf this way must be guarded by a sentry object.
-    // The sentry object performs various tasks,
-    // such as thread synchronization and updating the stream state.
-
-    std::istream::sentry se(is, true);
-    std::streambuf *sb = is.rdbuf();
-
-    for (;;) {
-        int c = sb->sbumpc();
-        switch (c) {
-        case '\n':
-            return is;
-        case '\r':
-            if (sb->sgetc() == '\n') {
-                sb->sbumpc();
-            }
-            return is;
-        case std::streambuf::traits_type::eof():
-            // Also handle the case when the last line has no line endings
-            if (t.empty()) {
-                is.setstate(std::ios::eofbit);
-            }
-            return is;
-        default:
-            t += (char)c;
-        }
-    }
 }
