d1385c57740b7973396d4166f9eb6b7209647286
Westlanderz <noah28129912@gmail.com>
2021-06-17 23:09:57 +0200

    Created an interface for between the bot and library

---
+++ include/botinterface.hpp
@@ -0,0 +1,27 @@
+#ifndef BOTINTERFACE_HPP
+#define BOTINTERFACE_HPP
+
+#include "include.hpp"
+
+/**
+ * @brief Creates a new shared pointer to a DppBot
+ * 
+ * @return std::shared_ptr<DppBot> 
+ */
+inline std::shared_ptr<DppBot> newBot(){
+    return std::make_shared<DppBot>();
+}
+
+class BotInterface {
+    private:
+        std::shared_ptr<DppBot> bot;
+    
+    public:
+        BotInterface() {
+            this->bot = newBot();
+        }
+        virtual ~BotInterface() = default;
+        std::shared_ptr<DppBot> isDppBot() { return bot; };
+};
+
+#endif // BOTINTERFACE_HPP
